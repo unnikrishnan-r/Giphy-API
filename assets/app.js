c77e22dc58684b5001ed9bd30faf3af17c382a25
Unni Ramachandran <unnipbvr@gmail.com>
2019-07-13 23:11:41 -0400

    Added user form on the page, when user enters text and submits a button is added to the page

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
   const apikey = "apikey=vT2XZqQdvN88TSL4h9wqV8pXsXJWj10d";
   const queryUrlBase = "https://api.giphy.com/v1/gifs/search?q=";
-  const countOfGifs = 10;
-  let animalArray = [
-    "lion",
-    "tiger",
-    "monkey",
-    "turtle",
-    "zebra",
-    "rhino",
-    "mouse",
-    "tiger",
-    "bear",
-    "gorilla"
+  const countOfGifs = 12;
+  let topicArray = [
+    "suits",
+    "louis litt",
+    "big bang theory",
+    "sheldon cooper",
+    "GOT",
+    "Friends",
+    "rachel",
+    "Walter White",
+    "Breaking Bad",
+    "Jesse Pinkman",
+    "leonardo dicaprio"
   ];
   /*Load Initial Buttons */
   function loadButtons(arrayOfTopics) {
@@ -22,7 +23,7 @@ $(document).ready(function() {
       $(".buttonArea").append(
         $("<button>", {
           type: "button",
-          class: "btn btn-success btn-md m-3 animalButton",
+          class: "btn btn-success btn-sm m-3 animalButton",
           text: arrayItem.toUpperCase()
         }).on("click", function() {
           //   console.log("Clicked Animal Button : " + $(this).text());
@@ -59,7 +60,7 @@ $(document).ready(function() {
     $(".gifArea").empty();
 
     for (var i = 0; i < giphyResponse.pagination.count; i++) {
-      var animatedUrl = giphyResponse.data[i].url;
+      var animatedUrl = giphyResponse.data[i].images.downsized.url;
       var staticUrl = giphyResponse.data[i].images.original_still.url;
       var gifRating = giphyResponse.data[i].rating;
       var gifTitle = giphyResponse.data[i].title;
@@ -73,19 +74,18 @@ $(document).ready(function() {
       //   console.log(userIdOfGif);
       //   console.log(giphyResponse.data[i].user.hasOwnProperty("disiplay_name"))
 
-      
       $(".gifArea").append(
         $("<div>", {
-          class: "col col-md-4"
+          class: "col-4 col-md-4 gifImageBlock justify-content-md-center"
         })
           .append(
             $("<img>", {
               src: staticUrl,
               alt: gifTitle,
               "data-state": "still",
-              class: " row gifImage",
-              "still-image" : staticUrl,
-              "animated-image" : animatedUrl
+              class: " row gifImage img-fluid justify-content-md-center",
+              "still-image": staticUrl,
+              "animated-image": animatedUrl
             })
           )
           .append(
@@ -103,29 +103,32 @@ $(document).ready(function() {
       );
 
       $(".gifImage").css({
-        width: "400px",
         padding: "10px",
+        margin: "10px",
+        width: "400px",
         height: "300px",
-        margin: "10px"
+        position: "relative"
       });
     }
     $(".gifImage").on("click", function() {
       console.log("Clicked an Image");
-      if ($(this).attr('data-state') === "still") {
-        console.log("Changing to Animate")
-        $(this).attr('data-state', 'animate');
-        // console.log($(this).attr('animated-image'));
-        $(this).attr('src', $(this).attr('animated-image'));
-
-      } else{
-        console.log("Changing to Still")
-
-        $(this).attr('data-state', 'still');
-        $(this).attr('src', $(this).attr('still-image'));
+      if ($(this).attr("data-state") === "still") {
+        $(this).attr("data-state", "animate");
+        $(this).attr("src", $(this).attr("animated-image"));
+      } else {
+        $(this).attr("data-state", "still");
+        $(this).attr("src", $(this).attr("still-image"));
       }
-
     });
   }
 
-  loadButtons(animalArray);
+  // This function handles events where one button is clicked
+  $("#add-topic").on("click", function(event) {
+    event.preventDefault();
+    topicArray.push($("#topic-input").val())
+    loadButtons(topicArray);
+  });
+
+
+  loadButtons(topicArray);
 });
